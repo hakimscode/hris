2c31b56f93eeb5da2cab3322780920079c1b4fe1
hakimscode <hhakimsetiawan@gmail.com>
2020-05-05 10:24:16 +0700

    change postgres to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [MongooseModule.forRoot('mongodb://localhost/hris')],
   controllers: [AppController],
   providers: [AppService]
 })
