5f64da44573ef06bc93295ca711f110641fcc844
hakimscode <hhakimsetiawan@gmail.com>
2020-07-24 06:06:31 +0700

    add companies menu

--- client/src/views/Data/Companies/Companies.js
+++ client/src/views/Data/Companies/Companies.js
@@ -3,10 +3,15 @@ import {
   Card,
   CardBody,
   CardHeader,
+  CardFooter,
   Col,
   Row,
   Table,
-  Button
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
 } from "reactstrap";
 import axios from "axios";
 
@@ -14,25 +19,163 @@ class Companies extends Component {
   constructor(props) {
     super(props);
 
-    this.API_URL = "http://localhost:5000/gudang_telurs";
-    // this.API_URL = "https://api.fawwazlab.com/lapor/api/jenis_laporan";
+    this.API_URL = "http://localhost:5001/companies";
 
     this.state = {
-      gudang_telurs: [
-        {
-          id: "",
-          jumlah: "",
-          tonase: "",
-        }
-      ],
+      companies: [],
+
+      name: "",
+      field: "",
+      address: "",
+
+      selectedId: "",
+      actionSubmit: "Simpan",
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    axios.get(this.API_URL).then(res => {
-      this.setState({ gudang_telurs: res.data.data });
-    });
+    axios
+      .get(this.API_URL, {
+        params: { componentType: this.state.componentType },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+        },
+      })
+      .then((res) => {
+        this.setState({ companies: res.data.data });
+      })
+      .catch((err) => console.log(err));
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  actionStatus = (companyId) => {
+    if (companyId !== "") {
+      axios
+        .get(this.API_URL + "/" + companyId, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+          },
+        })
+        .then((res) => {
+          this.setState({
+            selectedId: companyId,
+            name: res.data.data.name,
+            field: res.data.data.field,
+            address: res.data.data.address,
+          });
+        })
+        .catch((err) => console.log(err));
+    } else {
+      this.resetForm();
+    }
+  };
+
+  hapusClick = (companyId) => {
+    if (window.confirm("Anda yakin ingin menghapus data ini?")) {
+      axios
+        .delete(this.API_URL + "/" + companyId, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+          },
+        })
+        .then(() => {
+          this.setState({
+            companies: [
+              ...this.state.companies.filter(
+                (company) => company._id !== companyId
+              ),
+            ],
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: "",
+      field: "",
+      address: "",
+      selectedId: "",
+      actionSubmit: "Simpan",
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.selectedId === "") {
+      axios
+        .post(
+          this.API_URL,
+          {
+            name: this.state.name,
+            field: this.state.field,
+            address: this.state.address,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+            },
+          }
+        )
+        .then((res) => {
+          if (res.status === 201) {
+            this.setState({
+              companies: [...this.state.companies, res.data.data],
+            });
+            this.resetForm();
+          } else {
+            console.log("error");
+          }
+        })
+        .catch((err) => {
+          console.log(err.message);
+          console.log(this.state);
+        });
+    } else {
+      axios
+        .patch(
+          this.API_URL + "/" + this.state.selectedId,
+          {
+            name: this.state.name,
+            field: this.state.field,
+            address: this.state.address,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+            },
+          }
+        )
+        .then((res) => {
+          if (res.status === 202) {
+            this.setState({
+              companies: this.state.companies.map((company) => {
+                if (company._id === res.data.data._id) {
+                  company.name = res.data.data.name;
+                  company.field = res.data.data.field;
+                  company.address = res.data.data.address;
+                }
+                return company;
+              }),
+            });
+
+            this.resetForm();
+          } else {
+            console.log("error");
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  };
 
   render() {
     return (
@@ -41,23 +184,110 @@ class Companies extends Component {
           <Col xs="12" lg="12">
             <Card>
               <CardHeader>
-                <i className="fa fa-align-justify"></i> Data Gudang Telur
+                <i className="fa fa-align-justify"></i> Form Perusahaan
+              </CardHeader>
+              <Form onSubmit={this.handleSubmit} className="form-horizontal">
+                <CardBody>
+                  <FormGroup>
+                    <Row>
+                      <Col xs="6" md="6">
+                        <Label htmlFor="company-name">Nama Perusahaan</Label>
+                        <Input
+                          type="text"
+                          name="name"
+                          onChange={this.handleChange}
+                          value={this.state.name}
+                          required
+                          placeholder="Nama Perusahaan"
+                        />
+                      </Col>
+                      <Col xs="6" md="6">
+                        <Label htmlFor="field">Jenis Perusahaan</Label>
+                        <Input
+                          type="text"
+                          name="field"
+                          onChange={this.handleChange}
+                          value={this.state.field}
+                          required
+                          placeholder="Jumlah"
+                        />
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                  <FormGroup>
+                    <Row>
+                      <Col xs="12" md="12">
+                        <Label htmlFor="unit">Alamat</Label>
+                        <Input
+                          type="textarea"
+                          name="address"
+                          onChange={this.handleChange}
+                          value={this.state.address}
+                          rows="3"
+                          placeholder="Alamat"
+                          required
+                        ></Input>
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </CardBody>
+                <CardFooter>
+                  <Button type="submit" size="sm" color="primary">
+                    <i className="fa fa-dot-circle-o"></i>{" "}
+                    {this.state.actionSubmit}
+                  </Button>
+                  <Button
+                    size="sm"
+                    color="danger"
+                    onClick={this.actionStatus.bind(this, "")}
+                  >
+                    <i className="fa fa-ban"></i> Cancel
+                  </Button>
+                </CardFooter>
+              </Form>
+            </Card>
+          </Col>
+          <Col xs="12" lg="12">
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i> Data Perusahaan
               </CardHeader>
               <CardBody>
                 <Table responsive>
                   <thead>
                     <tr>
                       <th>No</th>
-                      <th>Jumlah Telur</th>
-                      <th>Tonase Telur</th>
+                      <th>Nama Perusahaan</th>
+                      <th>Jenis</th>
+                      <th>Alamat</th>
+                      <th>#</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {this.state.gudang_telurs.map((gudang_telur, index) => (
-                      <tr key={gudang_telur.id}>
+                    {this.state.companies.map((company, index) => (
+                      <tr key={company._id}>
                         <td>{index + 1}</td>
-                        <td>{gudang_telur.jumlah}</td>
-                        <td>{gudang_telur.tonase}</td>
+                        <td>{company.name}</td>
+                        <td>{company.field}</td>
+                        <td>{company.address}</td>
+                        <td>
+                          <Button
+                            size="sm"
+                            color="danger"
+                            className="mb-2 mr-1"
+                            onClick={this.hapusClick.bind(this, company._id)}
+                          >
+                            Hapus
+                          </Button>
+                          <Button
+                            size="sm"
+                            color="success"
+                            className="mb-2 mr-1"
+                            onClick={this.actionStatus.bind(this, company._id)}
+                          >
+                            Edit
+                          </Button>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
