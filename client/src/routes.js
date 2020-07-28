2f209e0baf6e571986ecb2c970486036ba1672b0
hakimscode <hhakimsetiawan@gmail.com>
2020-07-28 14:48:43 +0700

    separating component form for employee

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,16 +2,31 @@ import React from "react";
 const Dashboard = React.lazy(() => import("./views/Dashboard"));
 const Laporan = React.lazy(() => import("./views/Data/Laporan"));
 
-const KategoriProduk = React.lazy(() => import("./views/Data/KategoriProduk/KategoriProduk"));
+const KategoriProduk = React.lazy(() =>
+  import("./views/Data/KategoriProduk/KategoriProduk")
+);
 const Bonuses = React.lazy(() => import("./views/Master/Bonuses/Bonuses"));
-const Allowances = React.lazy(() => import("./views/Master/Allowances/Allowances"));
-const Deductions = React.lazy(() => import("./views/Master/Deductions/Deductions"));
+const Allowances = React.lazy(() =>
+  import("./views/Master/Allowances/Allowances")
+);
+const Deductions = React.lazy(() =>
+  import("./views/Master/Deductions/Deductions")
+);
 const Kandangs = React.lazy(() => import("./views/Data/Kandangs/Kandangs"));
 const Companies = React.lazy(() => import("./views/Data/Companies/Companies"));
 const Employees = React.lazy(() => import("./views/Data/Employees/Employees"));
-const DetailProject = React.lazy(() => import("./views/Data/Employees/DetailProject"));
-const Payrolls = React.lazy(() => import("./views/Transactions/Payrolls/Payrolls"));
-const Penjualan = React.lazy(() => import("./views/Transactions/Penjualan/Penjualan"));
+const EmployeeForm = React.lazy(() =>
+  import("./views/Data/Employees/EmployeeForm")
+);
+const EmployeeDetail = React.lazy(() =>
+  import("./views/Data/Employees/EmployeeDetail")
+);
+const Payrolls = React.lazy(() =>
+  import("./views/Transactions/Payrolls/Payrolls")
+);
+const Penjualan = React.lazy(() =>
+  import("./views/Transactions/Penjualan/Penjualan")
+);
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
@@ -22,52 +37,66 @@ const routes = [
   {
     path: "/data/KategoriProduk/",
     name: "Kategori Produk",
-    component: KategoriProduk
+    component: KategoriProduk,
   },
   {
     path: "/master/bonuses/",
     name: "Bonus",
-    component: Bonuses
+    component: Bonuses,
   },
   {
     path: "/master/allowances",
     name: "Tunjangan",
-    component: Allowances
+    component: Allowances,
   },
   {
     path: "/master/deductions",
     name: "Potongan",
-    component: Deductions
+    component: Deductions,
   },
   {
     path: "/data/Kandangs",
     name: "Kandang",
-    component: Kandangs
+    component: Kandangs,
   },
   {
     path: "/data/companies",
     name: "Perusahaan",
-    component: Companies
+    component: Companies,
   },
   {
     path: "/data/employees",
-    name: "Project",
-    component: Employees
+    name: "Pegawai",
+    exact: true,
+    component: Employees,
   },
   {
-    path: "/data/Project/:projectId",
-    name: "Detail Project",
-    component: DetailProject
+    path: "/data/employee/add",
+    name: "Tambah Pegawai",
+    exact: true,
+    component: EmployeeForm,
+  },
+  {
+    path: "/data/employee/:employeeId/edit",
+    name: "Edit Pegawai",
+    exact: true,
+    component: EmployeeForm,
+  },
+  {
+    path: "/data/employee/:employeeId",
+    name: "Detail Pegawai",
+    exact: true,
+    component: EmployeeDetail,
   },
   {
     path: "/transactions/payrolls",
     name: "Payrolls",
-    component: Payrolls
+    component: Payrolls,
   },
   {
     path: "/transactions/Penjualan",
     name: "Penjualan",
-    component: Penjualan
+    component: Penjualan,
   },
 ];
 
---
+++ client/src/views/Data/Employees/EmployeeForm.js
@@ -0,0 +1,474 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
+import axios from "axios";
+
+class EmployeeForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.API_URL = "http://localhost:5001/employees";
+    this.API_URL_COMPANIES = "http://localhost:5001/companies";
+
+    this.state = {
+      companies: [],
+
+      companyId: "",
+      idNumber: "",
+      name: "",
+      address: "",
+      gender: "",
+      placeOfBirth: "",
+      dateOfBirth: "",
+      maritalStatus: "",
+      phoneNumber: "",
+      email: "",
+      department: "",
+      role: "",
+      primarySalary: "",
+      dailyAllowance: "",
+
+      actionSubmit: "Simpan",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    axios
+      .get(this.API_URL_COMPANIES, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+        },
+      })
+      .then((res) => {
+        this.setState({ companies: res.data.data });
+      })
+      .catch((err) => console.log(err));
+
+    const url = this.props.match.url;
+    if (url.includes("edit")) {
+      const employeeId = this.props.match.params.employeeId;
+
+      axios
+        .get(this.API_URL + "/" + employeeId, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+          },
+        })
+        .then((res) => {
+          const companyId = res.data.data.companyId;
+          const {
+            idCardNumber,
+            name,
+            address,
+            gender,
+            placeOfBirth,
+            dateOfBirth,
+            maritalStatus,
+          } = res.data.data.profile;
+          const { phoneNumber, email } = res.data.data.contact;
+          const { department, role } = res.data.data.position;
+          const { primarySalary, dailyAllowance } = res.data.data.salary;
+
+          this.setState({
+            companyId,
+            idNumber: idCardNumber,
+            name,
+            address,
+            gender,
+            placeOfBirth,
+            dateOfBirth,
+            maritalStatus,
+            phoneNumber,
+            email,
+            department,
+            role,
+            primarySalary,
+            dailyAllowance,
+            actionSubmit: "Edit",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          alert("Data tidak ada");
+          // this.props.history.push("/data/employees");
+        });
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  resetForm = () => {
+    this.setState({
+      companyId: "",
+      idNumber: "",
+      name: "",
+      address: "",
+      gender: "",
+      placeOfBirth: "",
+      dateOfBirth: "",
+      maritalStatus: "",
+      phoneNumber: "",
+      email: "",
+      department: "",
+      role: "",
+      primarySalary: "",
+      dailyAllowance: "",
+    });
+  };
+
+  actionStatus = (employeeId) => {
+    if (employeeId !== "") {
+      axios
+        .get(this.API_URL + "/" + employeeId, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+          },
+        })
+        .then((res) => {
+          this.setState({
+            selectedId: employeeId,
+            name: res.data.data.name,
+            field: res.data.data.field,
+            address: res.data.data.address,
+          });
+        })
+        .catch((err) => console.log(err));
+    } else {
+      this.resetForm();
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.actionSubmit === "Simpan") {
+      axios
+        .post(
+          this.API_URL,
+          {
+            name: this.state.name,
+            field: this.state.field,
+            address: this.state.address,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+            },
+          }
+        )
+        .then((res) => {
+          if (res.status === 201) {
+            this.setState({
+              employees: [...this.state.employees, res.data.data],
+            });
+            this.resetForm();
+          } else {
+            console.log("error");
+          }
+        })
+        .catch((err) => {
+          console.log(err.message);
+          console.log(this.state);
+        });
+    } else {
+      axios
+        .patch(
+          this.API_URL + "/" + this.state.selectedId,
+          {
+            name: this.state.name,
+            field: this.state.field,
+            address: this.state.address,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("jwt-token-hris")}`,
+            },
+          }
+        )
+        .then((res) => {
+          if (res.status === 202) {
+            this.setState({
+              employees: this.state.employees.map((employee) => {
+                if (employee._id === res.data.data._id) {
+                  employee.name = res.data.data.name;
+                  employee.field = res.data.data.field;
+                  employee.address = res.data.data.address;
+                }
+                return employee;
+              }),
+            });
+
+            this.resetForm();
+          } else {
+            console.log("error");
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col xs="12" lg="12">
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i> Form Pegawai
+              </CardHeader>
+              <Form onSubmit={this.handleSubmit} className="form-horizontal">
+                <CardBody>
+                  <Row>
+                    <Col xs="4" md="4">
+                      <FormGroup>
+                        <Label htmlFor="field">NIK</Label>
+                        <Input
+                          type="text"
+                          name="idNumber"
+                          onChange={this.handleChange}
+                          value={this.state.idNumber}
+                          required
+                          placeholder="NIK"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col xs="6" md="6">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Nama Pegawai</Label>
+                        <Input
+                          type="text"
+                          name="name"
+                          onChange={this.handleChange}
+                          value={this.state.name}
+                          required
+                          placeholder="Nama Pegawai"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col xs="2" md="2">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Jenis Kelamin</Label>
+                        <Input
+                          type="select"
+                          name="gender"
+                          onChange={this.handleChange}
+                          value={this.state.gender}
+                          required
+                        >
+                          <option value="Laki-laki">Laki-laki</option>
+                          <option value="Perempuan">Perempuan</option>
+                        </Input>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col xs="4" md="4">
+                      <FormGroup>
+                        <Label htmlFor="field">Tempat Lahir</Label>
+                        <Input
+                          type="text"
+                          name="placeOfBirth"
+                          onChange={this.handleChange}
+                          value={this.state.placeOfBirth}
+                          required
+                          placeholder="Tempat Lahir"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col xs="4" md="4">
+                      <FormGroup>
+                        <Label htmlFor="field">Tanggal Lahir</Label>
+                        <Input
+                          type="text"
+                          name="dateOfBirth"
+                          onChange={this.handleChange}
+                          value={this.state.dateOfBirth}
+                          required
+                          placeholder="Tanggal Lahir"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col xs="4" md="4">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Status Kawin</Label>
+                        <Input
+                          type="select"
+                          name="maritalStatus"
+                          onChange={this.handleChange}
+                          value={this.state.maritalStatus}
+                          required
+                        >
+                          <option value="Single">Single</option>
+                          <option value="Married">Married</option>
+                        </Input>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col xs="6" md="6">
+                      <Row>
+                        <Col xs="12" md="12">
+                          <FormGroup>
+                            <Label htmlFor="field">Email</Label>
+                            <Input
+                              type="email"
+                              name="email"
+                              onChange={this.handleChange}
+                              value={this.state.email}
+                              required
+                              placeholder="Email"
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+
+                      <Row>
+                        <Col xs="12" md="12">
+                          <FormGroup>
+                            <Label htmlFor="field">No HP</Label>
+                            <Input
+                              type="text"
+                              name="phoneNumber"
+                              onChange={this.handleChange}
+                              value={this.state.phoneNumber}
+                              required
+                              placeholder="No HP"
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col xs="6" md="6">
+                      <FormGroup>
+                        <Label htmlFor="unit">Alamat</Label>
+                        <Input
+                          type="textarea"
+                          name="address"
+                          onChange={this.handleChange}
+                          value={this.state.address}
+                          rows="5"
+                          placeholder="Alamat"
+                          required
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col xs="6" md="6">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Perusahaan</Label>
+                        <Input
+                          type="select"
+                          name="companyId"
+                          onChange={this.handleChange}
+                          value={this.state.companyId}
+                          required
+                        >
+                          <option value="">-- pilih perusahaan --</option>
+                          {this.state.companies.map((company) => (
+                            <option key={company._id} value={company._id}>
+                              {company.name}
+                            </option>
+                          ))}
+                        </Input>
+                      </FormGroup>
+                    </Col>
+                    <Col xs="6" md="6">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Departemen</Label>
+                        <Input
+                          type="text"
+                          name="department"
+                          onChange={this.handleChange}
+                          value={this.state.department}
+                          required
+                          placeholder="Departemen"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col xs="4" md="4">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Jabatan</Label>
+                        <Input
+                          type="text"
+                          name="role"
+                          onChange={this.handleChange}
+                          value={this.state.role}
+                          required
+                          placeholder="Jabatan"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col xs="4" md="4">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Gaji Pokok</Label>
+                        <Input
+                          type="number"
+                          name="primarySalary"
+                          onChange={this.handleChange}
+                          value={this.state.primarySalary}
+                          required
+                          placeholder="Gaji Pokok"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col xs="4" md="4">
+                      <FormGroup>
+                        <Label htmlFor="employee-name">Uang Harian</Label>
+                        <Input
+                          type="number"
+                          name="dailyAllowance"
+                          onChange={this.handleChange}
+                          value={this.state.dailyAllowance}
+                          required
+                          placeholder="Uang Harian"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <Button type="submit" size="sm" color="primary">
+                    <i className="fa fa-dot-circle-o"></i>{" "}
+                    {this.state.actionSubmit}
+                  </Button>
+                  <Button
+                    size="sm"
+                    color="danger"
+                    onClick={this.actionStatus.bind(this, "")}
+                  >
+                    <i className="fa fa-ban"></i> Cancel
+                  </Button>
+                </CardFooter>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default EmployeeForm;
